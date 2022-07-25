9185c10f31f9f22bc4ac55117a0085d76e6aa40f
Joel Jos Joseph <joeljosjoseph12@gmail.com>
2022-07-25 22:57:00 +0530

    Created navbar which displays the netflix logo along with the user avatar. Fade in and out properties were also addded through nav.css file along with other styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import requests from "./requests";
 import Row from "./Row";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+
+function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
